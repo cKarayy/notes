2f65d56bae9b9adb7dc6a905e6f38927da9f409d
karay <callistavirginia88@gmail.com>
2024-04-22 10:11:08 +0700

    jumat kemarin

--- lib/screens/note_list_screen.dart
+++ lib/screens/note_list_screen.dart
@@ -1,7 +1,6 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class NoteListScreen extends StatefulWidget {
   const NoteListScreen({super.key});
@@ -93,35 +92,111 @@ class NotesList extends StatelessWidget {
         if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         }
-        return ListView(
-          padding: const EdgeInsets.only(bottom: 80),
-          children: snapshot.data.docs.map(
-            (document) {
-              return Card(
-                child: ListTile(
-                  title: Text(document['title']),
-                  subtitle: Text(document['description']),
-                  trailing: InkWell(
-                    onTap: () {
-                      FirebaseFirestore.instance
-                          .collection('notes')
-                          .doc(document.id)
-                          .delete()
-                          .catchError((e) {
-                        print(e);
-                      });
-                    },
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: Icon(Icons.delete),
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          default:
+            return ListView(
+              padding: const EdgeInsets.only(bottom: 80),
+              children: snapshot.data!.docs.map(
+                (document) {
+                  return Card(
+                    child: ListTile(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            TextEditingController titleController =
+                                TextEditingController(text: document['title']);
+                            TextEditingController descriptionController =
+                                TextEditingController(
+                                    text: document['description']);
+                            return AlertDialog(
+                              title: const Text('Update Data Notes'),
+                              content: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text('Title: ', textAlign: TextAlign.start),
+                                  TextField(
+                                    controller: titleController,
+                                    // decoration: InputDecoration(
+                                    //   hintText: document['title'],
+                                    // ),
+                                  ),
+                                  const Padding(
+                                    padding: EdgeInsets.only(top: 10),
+                                    child: Text('Description: ',
+                                        textAlign: TextAlign.start),
+                                  ),
+                                  TextField(
+                                    controller: descriptionController,
+                                    // decoration: InputDecoration(
+                                    //   hintText: document['description'],
+                                    // ),
+                                  )
+                                ],
+                              ),
+                              actions: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('Cancel'),
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Map<String, dynamic> updateNote = {};
+                                    updateNote['title'] = titleController.text;
+                                    updateNote['description'] =
+                                        descriptionController.text;
+
+                                    FirebaseFirestore.instance
+                                        .collection('notes')
+                                        .doc(document.id)
+                                        .update(updateNote)
+                                        .whenComplete(
+                                      () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    );
+                                  },
+                                  child: const Text('update'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      title: Text(document['title']),
+                      subtitle: Text(document['description']),
+                      trailing: InkWell(
+                        onTap: () {
+                          FirebaseFirestore.instance
+                              .collection('notes')
+                              .doc(document.id)
+                              .delete()
+                              .catchError((e) {
+                            print(e);
+                          });
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          child: Icon(Icons.delete),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              );
-            },
-          ),
-        );
+                  );
+                },
+              ).toList(),
+            );
+        }
       },
     );
   }
